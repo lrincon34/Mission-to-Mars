c0e9b28ea5004b18362f5dc88b4d59efd73f8483
Lizett Rincon <lrincon34@gmail.com>
2020-08-06 23:24:49 -0600

    adding index.html

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
-from splinter import Browser
 from flask import Flask, render_template
 from flask_pymongo import PyMongo
-from bs4 import BeautifulSoup as soup 
-import requests
-import pandas as pd
 import scraping
-
 
 app = Flask(__name__)
 
@@ -13,12 +8,10 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
-
 @app.route("/")
 def index():
    mars = mongo.db.mars.find_one()
    return render_template("index.html", mars=mars)
-
 
 @app.route("/scrape")
 def scrape():
@@ -27,6 +20,5 @@ def scrape():
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
 
-
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
